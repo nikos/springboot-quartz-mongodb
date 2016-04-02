f8b71c1e5e786ebcb4d579bb829e30670fbafab9
Niko Schmuck <niko@nava.de>
2016-04-02 23:05:15 +0200

    Verify if trigger is already registered in job store, otherwise skip configuration.

--- src/main/java/de/nava/demo/config/JobConfiguration.java
+++ src/main/java/de/nava/demo/config/JobConfiguration.java
@@ -3,19 +3,13 @@ package de.nava.demo.config;
 import de.nava.demo.job.MyJobOne;
 import de.nava.demo.job.MyJobTwo;
 import org.joda.time.DateTime;
-import org.quartz.JobDataMap;
-import org.quartz.JobDetail;
-import org.quartz.JobKey;
-import org.quartz.Trigger;
+import org.quartz.*;
 import org.quartz.impl.JobDetailImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import javax.annotation.PostConstruct;
-
-import java.util.Arrays;
-import java.util.HashSet;
 
 import static org.quartz.CronScheduleBuilder.cronSchedule;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
@@ -30,18 +24,14 @@ public class JobConfiguration {
     @PostConstruct
     private void initialize() throws Exception {
         schedulerFactoryBean.getScheduler().addJob(jobDetailMyJobOne(), true, true);
-        schedulerFactoryBean.getScheduler().scheduleJob(simpleTriggerMyJobOne());
+        if (!schedulerFactoryBean.getScheduler().checkExists(new TriggerKey("trigger1", "mygroup"))) {
+            schedulerFactoryBean.getScheduler().scheduleJob(simpleTriggerMyJobOne());
+        }
 
         schedulerFactoryBean.getScheduler().addJob(jobDetailMyJobTwo(), true, true);
-        schedulerFactoryBean.getScheduler().scheduleJob(cronTriggerMyJobTwo());
-        /*
-          schedulerFactoryBean.getScheduler().scheduleJob(jobDetailMyJobTwo(),
-                new HashSet<>(Arrays.asList(cronTriggerMyJobTwo())), true);
-          throws:
-              caused by: java.lang.UnsupportedOperationException: null
-	            at com.novemberain.quartz.mongodb.MongoDBJobStore.storeJobsAndTriggers(MongoDBJobStore.java:172) ~[quartz-mongodb-2.0.0-rc1.jar:na]
-	            at org.quartz.core.QuartzScheduler.scheduleJobs(QuartzScheduler.java:1056) ~[quartz-2.2.2.jar:na]
-        */
+        if (!schedulerFactoryBean.getScheduler().checkExists(new TriggerKey("trigger2", "mygroup"))) {
+            schedulerFactoryBean.getScheduler().scheduleJob(cronTriggerMyJobTwo());
+        }
     }
 
     private static JobDetail jobDetailMyJobOne() {

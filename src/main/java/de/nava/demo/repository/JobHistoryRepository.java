265aecd16b3826cd2af64dbc6d50efe2802689fb
Niko Schmuck <niko@nava.de>
2016-04-09 20:00:03 +0200

    Wiring mongo client.

--- src/main/java/de/nava/demo/repository/JobHistoryRepository.java
+++ src/main/java/de/nava/demo/repository/JobHistoryRepository.java
@@ -3,25 +3,27 @@ package de.nava.demo.repository;
 import com.mongodb.MongoClient;
 import org.bson.Document;
 import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
 
 /**
+ * Simplest possible implementation for accessing the MongoDB
+ * to persist job history documents.
+ * 
  * @author Niko Schmuck
  */
 @Repository
 public class JobHistoryRepository {
 
+    @Autowired
     private MongoClient mongo;
 
-
     public void add(Map<String, Object> keys) {
-        if (mongo != null) {
-            // TODO: make configurable / use repository ?
-            // TODO: set expire TTL based on 'ts' field (ie. 7 days)
-            mongo.getDatabase("jobs-demo").getCollection("job_history")
-                    .insertOne(new Document(keys));
-        }
+        // TODO: make configurable / use repository ?
+        // TODO: set expire TTL based on 'ts' field (ie. 7 days)
+        mongo.getDatabase("jobs-demo").getCollection("job_history")
+                .insertOne(new Document(keys));
     }
 
 }
